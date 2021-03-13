b6f1ff6317c330f91a80d6f0b9954488a7cf9b4a
racyriaz <racyriaz@gmail.com>
2021-03-13 15:42:18 +0530

    nav

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import "./Stylesheet.css";
 function Home() {
 	return (
-		<div>
-			<h1>Home page</h1>
-			<h1>page contents</h1>
+		<div className="view-container">
+			<div className="flex row">
+				<h1>Home page</h1>
+				<h1>page contents</h1>
+			</div>
 		</div>
 	);
 }
