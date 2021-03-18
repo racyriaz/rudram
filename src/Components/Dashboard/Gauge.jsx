1c23293612e23497c933483eec91aeec15e4b792
racyriaz <racyriaz@gmail.com>
2021-03-18 20:40:54 +0530

    gauge

--- src/Components/Dashboard/Gauge.jsx
+++ src/Components/Dashboard/Gauge.jsx
@@ -2,14 +2,26 @@ import React from "react";
 import GaugeChart from "react-gauge-chart";
 import "../../Views/Stylesheet.css";
 
-function Gauge({ id, percent }) {
+function Gauge({ id, percentage }) {
 	const chartStyle = {
-		height: 250,
+		height: "auto",
+		width: "150px",
+		color: "black",
+		zIndex: 5,
 	};
 	return (
-		<div className="flex col">
-			<h6>Air Quality Gauge Chart</h6>
-			<GaugeChart id={id} style={chartStyle} percent={percent} animDelay={0} />
+		<div className="flex col justify-center">
+			<GaugeChart
+				id={id}
+				nrOfLevels={5}
+				style={chartStyle}
+				colors={["#FF5F6D", "#FFC371", "#3de260"]}
+				textColor="#d49d42"
+				percent={percentage}
+				animate={true}
+				marginInPercent="0.025"
+				cornerRadius="5"
+			/>
 		</div>
 	);
 }
--- src/Views/AirQualitySubComponents/Dashboard.jsx
+++ src/Views/AirQualitySubComponents/Dashboard.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import "../Stylesheet.css";
+import Gauge from "../../Components/Dashboard/Gauge";
 
 function Dashboard() {
 	return (
-		<div className="flex row mg-lr-2rem">
-			<h4 className="flex justify-center pad-10px font-2-5rem">AQI</h4>
-			<div className="pad-10px">
-				An air quality index (AQI) is parameter that used while communicating on
-				how polluted the air currently is or how polluted it is forecast to
-				become. Public health risks increase as the AQI rises. Different
-				countries have their own air quality indices, corresponding to different
-				national air quality standards.
+		<div className="flex col mg-lr-2rem">
+			<div className="flex row">
+				<h4 className="flex justify-center pad-10px font-2-5rem">AQI</h4>
+				<div className="pad-10px">
+					An air quality index (AQI) is parameter that used while communicating
+					on how polluted the air currently is or how polluted it is forecast to
+					become. Public health risks increase as the AQI rises. Different
+					countries have their own air quality indices, corresponding to
+					different national air quality standards.
+				</div>
+				<div className="flex col mg-lr-1rem">
+					<Gauge id="1" percentage="0.9" />
+					<h6 style={{ display: "block", textAlign: "center" }}>
+						API GAUGE CHART
+					</h6>
+				</div>
 			</div>
+			<div className="flex row"></div>
 		</div>
 	);
 }
