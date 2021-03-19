82ae55a1fae66d1af67f853bebce6a1456bbc4a5
racyriaz <racyriaz@gmail.com>
2021-03-19 12:12:47 +0530

    stings upper case

--- src/Components/ParamsCard.jsx
+++ src/Components/ParamsCard.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "../Components/cardstyle.css";
 function ParamsCard({ parameter, value }) {
+	var pollutant = parameter.toUpperCase();
+	if (pollutant === "PM2_5") {
+		pollutant = "PM 2.5";
+	} else if (pollutant === "ROOM_TEMP") {
+		pollutant = "TEMPERATURE ";
+		value = value + " °C";
+	}
+
 	return (
 		<div className="paramsCard">
 			<div className="subParam1">{value}</div>
-			<div className="subParam2">{parameter}</div>
+			<div className="subParam2">{pollutant}</div>
 		</div>
 	);
 }
--- src/Components/WeatherCard.jsx
+++ src/Components/WeatherCard.jsx
@@ -3,6 +3,7 @@ import "../Views/Stylesheet.css";
 import "./cardstyle.css";
 
 function WeatherCard({ content, color }) {
+	content = content.charAt(0).toUpperCase() + content.slice(1);
 	return (
 		<div
 			className="weather-card flex justify-center"
